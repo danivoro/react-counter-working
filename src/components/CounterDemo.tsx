5686931bc6e963c82fdf4ec532380fd1fcec8ebb
dani voro <mr.voro@gmail.com>
2023-07-04 20:44:46 +0200

    fixing the app and adding three buttons

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -8,8 +8,20 @@ function CounterDemo(): JSX.Element {
     queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
   };
 
+  const handleAddFive = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 5);
+  };
+
+  const handleSubstractFive = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 5);
+  };
+
+  const handleSubstractOne = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 1);
+  };
+
   const handleReset = () => {
-    // TODO reset counter to 0 here
+    queueRerenderWithNewCounterValue(0)
   }
 
   return (
@@ -17,6 +29,9 @@ function CounterDemo(): JSX.Element {
       <h1>Counter Demo</h1>
       <p>Current value: {counterValueFromCurrentRender}</p>
       <button onClick={handleAddOne}>+1</button>
+      <button onClick={handleAddFive}>+5</button>
+      <button onClick={handleSubstractOne}>-1</button>
+      <button onClick={handleSubstractFive}>-5</button>
       <hr />
       <button onClick={handleReset}>Reset counter</button>
     </>
